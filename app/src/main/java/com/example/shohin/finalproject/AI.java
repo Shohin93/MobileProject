53a829d832297761244b8277afcd43a4ac09efed
Shohin <shohin26@gmail.com>
2016-12-05 13:29:12 -0500

    AI class

--- app/src/main/java/com/example/shohin/finalproject/AI.java
+++ app/src/main/java/com/example/shohin/finalproject/AI.java
@@ -1,8 +1,28 @@
 package com.example.shohin.finalproject;
 
-/**
- * Created by shohin on 12/5/16.
- */
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
 public class AI {
-    
+    private static final int BOMBSCOUNT = 13; // bomb count includes 0
+    private ArrayList<Coordinate> aiCoordinates;
+    private int numberOfBombs;
+
+    public AI() {
+        this.numberOfBombs = BOMBSCOUNT;
+        this.aiCoordinates = new ArrayList<>();
+    }
+
+    public ArrayList<Coordinate> getAiCoordinates() {
+        return this.aiCoordinates;
+    }
+
+    public int getNumberOfBombs() {
+        return this.numberOfBombs;
+    }
+
+    public void decrementNumberOfBombs() {
+        this.numberOfBombs--;
+    }
 }
